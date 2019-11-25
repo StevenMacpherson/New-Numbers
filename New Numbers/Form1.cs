4fe0ad24c343d781891a64a8ef7e7e59176f4695
Steven Macpherson <rc0039062@student.riversidecollege.ac.uk>
2019-11-25 15:15:49 +0000

    Added Pi

--- New Numbers/Form1.cs
+++ New Numbers/Form1.cs
@@ -24,6 +24,13 @@ namespace New_Numbers
             richTextBox1.AppendText(d1.ToString("0.0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.0E0") + "\n");
             richTextBox1.AppendText(d1.ToString("0.00E00") + "\n");
+            double pi = 3.141592653589793;
+            richTextBox1.AppendText(pi.ToString("N3") + "\n");
+            richTextBox1.AppendText(pi.ToString("N5") + "\n");
+            richTextBox1.AppendText(pi.ToString("N15") + "\n");
+
+
+
 
 
         }
